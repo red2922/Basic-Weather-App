eb48d071194a2d0dd48354462b08da736d0ec8c1
red2922 <jinthavongsa1@gmail.com>
2024-12-09 17:45:59 -0600

    Poggers

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const card1 = document.getElementById("card1");
+var testing = document.getElementById("testing");
 const weatherData = {};
 
 async function getWeatherData() {
-    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      weatherData = await response.json();
-    } catch (error) {
-      console.error(error.message);
+  const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
-  }
 
+    weatherData = await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 console.log(weatherData);
+
+function tryingStuff() {
+  testing.innerHTML = "<div>WOZERS</div>";
+}
 
 card1.textContent = `${weatherData.daily.time[0]}`;
