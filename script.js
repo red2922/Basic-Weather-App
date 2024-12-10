1829b2d10bc242e54c29194c7713c28c20a5e13f
red2922 <jinthavongsa1@gmail.com>
2024-12-09 20:32:31 -0600

    Got Weekday working

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-const card1 = document.getElementById("card1");
+const date = new Date();
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 var testing = document.getElementById("testing");
-const weatherData = {};
+var weatherData = {};
+
+const url =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver";
 
 async function getWeatherData() {
-  const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver";
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     weatherData = await response.json();
   } catch (error) {
     console.error(error.message);
   }
 }
 
-console.log(weatherData);
-
-function tryingStuff() {
-  testing.innerHTML = "<div>WOZERS</div>";
+function setWeekDayCards() {
+  let firstDay = date.getDay();
+  for (let i = 0; i < 5; i++) {
+    document.getElementById(`card${i + 1}`).innerText =
+      weekDays[(i + firstDay) % 7];
+  }
 }
 
-card1.textContent = `${weatherData.daily.time[0]}`;
+getWeatherData();
+
+function tryingStuff() {
+  setWeekDayCards();
+  console.log(weatherData);
+  testing.innerHTML = `<div>${weatherData.daily.temperature_2m_max[0]}</div>`;
+}
